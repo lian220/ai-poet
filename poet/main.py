bbbb790ffe40f7c3c334dd089b02b2bcbb007a98
lian220 <lian.dy220@gmail.com>
2025-02-09 16:36:03 +0900

    fix. import 소스 추가

--- poet/main.py
+++ poet/main.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # from langchain.llms import OpenAI
 # llm = OpenAI()
@@ -19,7 +19,7 @@ llm = ChatOpenAI()
 st.title('인공지능 시인')
 content = st.text_input("시의 주제를 제시해 주세요.")
 if st.button("시 작성 요청하기"):
-    with st.spinner("시 작성 중...", show_time=True):
+    with st.spinner("시 작성  중...", show_time=True):
         result = llm.predict(content + "에 대한 시를 써줘")
         st.write(result)
 
